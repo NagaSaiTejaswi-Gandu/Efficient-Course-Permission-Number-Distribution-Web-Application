a8b7bfdae81386af31f590238496b07a3883b451
NagaSaiTejaswi-Gandu <113472403+NagaSaiTejaswi-Gandu@users.noreply.github.com>
2022-12-02 20:27:17 -0500

    Final Project

--- IWS-Project/permo-main/app/models.py
+++ IWS-Project/permo-main/app/models.py
@@ -8,33 +8,59 @@ from app import db, login
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-# request = db.Table('requests',
-#     db.Column('id', db.Integer, primary_key=True),
-#     db.Column('status', db.Boolean, index = True),
-#     db.Column('student_id', db.Integer, db.ForeignKey('user.id'), index=True),
-#     db.Column('course_id', db.Integer, db.ForeignKey('college_course.id'), index=True)
-# )   
+
+
+class PermissionNumbers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    permission_number = db.Column(db.Integer, index=True)
+    assigned = db.Column(db.Boolean, index=True)
+    course_name = db.Column(db.String, index = True)
+    course_id = db.Column(db.Integer, index= True)
+    professor_id = db.Column(db.Integer, index = True)
+    student_id = db.Column(db.Integer, index=True)
+    request_id = db.Column(db.Integer, index=True)
+
+
+class CollegeCourse(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    course_name = db.Column(db.String(20))
+    professor_id = db.Column(db.Integer, index=True)
+    
+    def __repr__(self):
+        return '<CollegeCourse {}'.format(self.course_name)
+    
+class pn_requests(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    status = db.Column( db.Boolean, index = True, default = False)
+    message = db.Column(db.String(512))
+    class_name = db.Column( db.String(64), index=True)
+    course_id = db.Column( db.Integer, index=True)
+    professor_id = db.Column( db.Integer, index=True)
+    student_id = db.Column( db.Integer, index=True)
+    owner = db.Column(db.Integer, db.ForeignKey('user.id'), index=True)
+    permission_number = db.Column( db.Integer)
+    date = db.Column( db.DateTime, default=datetime.utcnow)
+
+
+    def approve(self):
+        self.status = True
+    
+    
 
 
 class User(UserMixin, db.Model):
     firstName = db.Column(db.String(64), index=True)
     lastName = db.Column(db.String(64), index=True)
     id = db.Column(db.Integer, primary_key=True)
+    is_professor = db.Column(db.Boolean)
     username = db.Column(db.String(64), index=True, unique=True, )
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    courses = db.relationship('CollegeCourse', backref='professor', lazy='dynamic')
     about_me = db.Column(db.String(140))
     location = db.Column(db.String(140))
-    last_seen = db.Column(db.DateTime, default=datetime.utcnow  )
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow )
     verified = db.Column(db.Boolean, default=False)
     verified_on = db.Column(db.DateTime, default=datetime.utcnow)
-    # requests = db.relationship('User', 
-    # secondary = request, 
-    # primaryjoin=(request.c.course_id == id),
-    # secondaryjoin=(request.c.student_id == id),
-    # backref=db.backref('requests', lazy='dynamic'), lazy='dynamic')
-
 
 
     def __repr__(self):
@@ -65,6 +91,8 @@ class User(UserMixin, db.Model):
             return
         return User.query.get(id)
     
+
+
     
 
 @login.user_loader### Do we need this??
@@ -72,45 +100,9 @@ def load_user(id):
     return User.query.get(int(id))
 
 
-class Professor(User):
-     courses = db.relationship('CollegeCourse', backref='professor', lazy='dynamic')
-
-class Student(User):
-    pass
-
-
-
-
 ### Work in progress.
 
 ### timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow) 
-
-class CollegeCourse(db.Model):
-    __tablename__ = 'college_course'
-    id = db.Column(db.Integer, primary_key = True)
-    course_name = db.Column(db.String(20))
-    professor_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    # permission_numbers = db.relationship('permission_numbers', backref='course')
-    # requests = db.relationship('college_course', 
-    # secondary = request, 
-    # primaryjoin=(request.c.course_id == id),
-    # secondaryjoin=(request.c.student_id == id),
-    # backref=db.backref('requests', lazy='dynamic'), lazy='dynamic')
-
-    
-
-
-    def __repr__(self):
-        return '<CollegeCourse {}'.format(self.course_name)
-    
-# permission_numbers = db.Table('permission_numbers',
-#     db.Column('id', db.Integer, primary_key = True),
-#     db.Column('permission_number', db.Integer),
-#     db.Column('Assigned', db.Boolean, index=True),
-#     db.Column('course_name', db.String, db.ForeignKey('college_course.course_name'), index=True),
-#     db.Column('student_id', db.Integer, db.ForeignKey('user.id'), index=True)
-# )
-
 
 
 
